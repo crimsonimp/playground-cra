a870f7956dcb330e1a22bbf592c97c7abf24a0ff
Volodymyr Bakay <volodymyr.bakay@gmail.com>
2020-05-19 19:26:43 +0300

    Updates

---
+++ precache-manifest.302563d73559b4e8462616ed2d3052f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a61503191de92d3517976c6def53bd7",
+    "url": "/playground-cra/index.html"
+  },
+  {
+    "revision": "385fb64bbcdd34e29cdb",
+    "url": "/playground-cra/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a11eed5ae24a67cbadc4",
+    "url": "/playground-cra/static/js/2.480992be.chunk.js"
+  },
+  {
+    "revision": "fdb2bbbbb647ba518ef54493c5cba538",
+    "url": "/playground-cra/static/js/2.480992be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385fb64bbcdd34e29cdb",
+    "url": "/playground-cra/static/js/main.075ff8b5.chunk.js"
+  },
+  {
+    "revision": "3b73fde983bf4cc27f61",
+    "url": "/playground-cra/static/js/runtime-main.f27dc6a8.js"
+  }
+]);
