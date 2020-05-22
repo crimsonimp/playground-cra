3662ee6e4e5a0dcdd027eaa85256ff7b0c920c7a
Volodymyr Bakay <volodymyr.bakay@gmail.com>
2020-05-22 11:10:03 +0300

    Updates

---
+++ precache-manifest.15396b10ef671fa10badf3f1eb2b8707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1410413b86ace741fcc93205b176b5a",
+    "url": "/playground-cra/index.html"
+  },
+  {
+    "revision": "afb819c9088514468ad9",
+    "url": "/playground-cra/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ab82c35c992b6a9b1bd6",
+    "url": "/playground-cra/static/js/2.b90ad7ea.chunk.js"
+  },
+  {
+    "revision": "8b04f22a06eb6f0b0d0009c4a67b3596",
+    "url": "/playground-cra/static/js/2.b90ad7ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb819c9088514468ad9",
+    "url": "/playground-cra/static/js/main.923edf5a.chunk.js"
+  },
+  {
+    "revision": "3b73fde983bf4cc27f61",
+    "url": "/playground-cra/static/js/runtime-main.f27dc6a8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/playground-cra/precache-manifest.5f2bc74f3d130299440e006f56997ea2.js"
+  "/playground-cra/precache-manifest.15396b10ef671fa10badf3f1eb2b8707.js"
 );
 
 self.addEventListener('message', (event) => {
